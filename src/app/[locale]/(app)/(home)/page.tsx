a31584fad2b65974a8a60abbfcff8c80314122a7
Leo <leo@8u8.cc>
2026-01-01 20:30:29 +0400

    fix: 测试滚动

--- src/app/[locale]/(app)/(home)/page.tsx
+++ src/app/[locale]/(app)/(home)/page.tsx
@@ -3,21 +3,23 @@ import { useEffect } from "react";
 import Button from "@/components/ui/Button";
 import { useRouter } from "@/i18n/navigation";
 import { Routes } from "@/lib/routes";
-
 export default function HomePage() {
   const router = useRouter();
   useEffect(() => {
     console.log("Home Page Loaded");
   }, []);
   return (
-    <div className="p-4 flex gap-2 flex-wrap">
-      <Button onClick={() => router.push(Routes.I18n)}>国际化</Button>
-      <Button onClick={() => router.push(Routes.ThemeAndSkin)}>主题皮肤</Button>
-      <Button onClick={() => router.push(Routes.DynamicComps)}>组件动态导入</Button>
-      <Button onClick={() => router.push(Routes.ModalProfile)}>路由弹框</Button>
-      <Button onClick={() => router.push(Routes.Dialog)}>普通弹框</Button>
-      <Button onClick={() => router.push(Routes.Motion)}>Motion</Button>
-      <Button onClick={() => router.push(Routes.CdnImage)}>CDN图片参数优化</Button>
+    <div data-name="HomePage">
+      <div className="p-4 flex gap-2 flex-wrap">
+        <Button onClick={() => router.push(Routes.I18n)}>国际化</Button>
+        <Button onClick={() => router.push(Routes.ThemeAndSkin)}>主题皮肤</Button>
+        <Button onClick={() => router.push(Routes.DynamicComps)}>组件动态导入</Button>
+        <Button onClick={() => router.push(Routes.ModalProfile)}>路由弹框</Button>
+        <Button onClick={() => router.push(Routes.Dialog)}>普通弹框</Button>
+        <Button onClick={() => router.push(Routes.Motion)}>Motion</Button>
+        <Button onClick={() => router.push(Routes.CdnImage)}>CDN图片参数优化</Button>
+      </div>
+      <div className="mt-4 w-[300px] h-[2000px] mx-auto bg-pink-400"></div>
     </div>
   );
 }
